2ab58e4beed0b9d73e1d75017f17e536d047639e
John Oerter <john@johnoerter.me>
2020-07-16 07:34:25 -0500

    Change logic to calculate time remaining from end time

--- src/cljs/tomate/goal-timer.cljs
+++ src/cljs/tomate/goal-timer.cljs
@@ -2,19 +2,26 @@
   (:require
    [reagent.core :as reagent :refer [atom]]))
 
-(defn get-minutes [seconds]
-  (.padStart (str (js/Math.floor (/ seconds 60))) 2 "0"))
+(defn get-minutes [milliseconds]
+  (.padStart (str (js/Math.floor (/ milliseconds 60000))) 2 "0"))
 
-(defn get-seconds [total-seconds]
-  (.padStart (str (mod total-seconds 60)) 2 "0"))
+(defn get-seconds [milliseconds]
+  (.padStart (str (mod milliseconds 60000)) 2 "0"))
 
-(defn seconds->time [total-seconds]
-  (str (get-minutes total-seconds) ":" (get-seconds total-seconds)))
+(defn milliseconds->time [total-milliseconds]
+  (str (get-minutes total-milliseconds) ":" (get-seconds total-milliseconds)))
+
+(defn get-end-time [] (+ 1500000 (.getTime (js/Date.))))
+
+(defn milliseconds-from-end-time [end-time]
+  (let [current-time (.getTime (js/Date.))]
+    (- end-time current-time)))
 
 (defn goal-timer [the-goal]
-  (let [total-seconds (atom 1500)]
+  (let [total-milliseconds (atom 1500000) end-time (get-end-time)]
     (fn []
-      (js/setTimeout #(swap! total-seconds dec) 1000)
+      ;; (js/console.log "seconds from end time:" (seconds-from-end-time end-time))
+      (js/setTimeout #(reset! total-milliseconds (milliseconds-from-end-time end-time)) 1000)
       [:div.timer
        [:h1 @the-goal]
-       [:h1 (seconds->time @total-seconds)]])))
+       [:h1 (milliseconds->time @total-milliseconds)]])))
